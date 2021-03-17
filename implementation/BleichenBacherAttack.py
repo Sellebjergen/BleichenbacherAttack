d40d24b9ebfc4671306a286fcb8c2151654461f1
sellebjergen <sellebjergen@gmail.com>
2021-03-17 16:39:05 +0100

    It seems like I have phase 2.a working by now.

--- implementation/BleichenBacherAttack.py
+++ implementation/BleichenBacherAttack.py
@@ -1,5 +1,5 @@
 from Crypto.Util.number import bytes_to_long, long_to_bytes
-
+import numpy as np
 
 # TODO: Checking the type of the oracle, if we give it an url, make a request instead.
 # TODO: Can we search after for values in the blinding phase more efficiently?
@@ -9,38 +9,59 @@ class BleichenBacherAttack:
     def __init__(self, cipher_bytes, oracle, rsa):
         self.cipher_bytes = cipher_bytes
         self.rsa = rsa
-
         self.oracle = oracle
 
-    def call_oracle(self, lower_bound, upper_bound):
+    def blinding_phase(self, lower_bound, upper_bound):
+        print("starting blinding phase")
         e = self.rsa.get_public_key().e
         n = self.rsa.get_public_key().n
         c = bytes_to_long(self.cipher_bytes)
-
+        i = 1
         for s_i in range(lower_bound, upper_bound):
-            if s_i % 1000000 == 0:
-                print(s_i)
-            c_i = (c * pow(s_i, e, n)) % n
-            c_i_bytes = long_to_bytes(c_i)
-            if self.oracle.get_conforming_status(c_i_bytes):
-                print("found a value")
+            if s_i % 100000 == 0:
+                print("I'm still alive, don't worry about it man...")
 
-        print("searching has completed")
+            c_0 = (c * pow(s_i, e, n)) % n
+            c_0_bytes = long_to_bytes(c_0)
+
+            if self.oracle.get_conforming_status(c_0_bytes):
+                print("found a value")
+                return c_0, s_i, i
+
+        # if nothing was found i return None.
+        return None
+
+    def phase2_a(self, c_0, s_i, i):
+        B = 2 ** (8 * (self.rsa.amount_of_bits - 2))
+        e = self.rsa.get_public_key().e
+        n = self.rsa.get_public_key().n
+
+        if i == 1:
+            s_1 = n // 3*B + 1
+            math = c_0 * pow(s_1, e, n) % n
+            math_bytes = long_to_bytes(math)
+            while not self.oracle.get_conforming_status(math_bytes):
+                s_1 += 1
+                math = c_0 * pow(s_1, e, n) % n
+                math_bytes = long_to_bytes(math)
+            print("We've found the next value in the interval.")
+            return s_1
 
     def run(self):
         B = 2 ** (8 * (self.rsa.amount_of_bits - 2))
         lower_bound = 2*B
         upper_bound = 3*B - 1
-        e = self.rsa.get_public_key().e
-        n = self.rsa.get_public_key().n
 
         # step 1.   blinding
-        self.call_oracle(lower_bound, upper_bound)
+        c_0, s_i, i = self.blinding_phase(lower_bound, upper_bound)
 
+        print("jumping to phase 2 instead.")
+        s_1 = self.phase2_a(c_0, s_i, i)
+
+        print(s_1)
         # step 2.   searching for PKCS conforming messages
         # step 2.a  starting the search
         # step 2.b  searching with more than one interval
         # step 2.c  searching with one interval left
         # step 3    narrowing the set of solutions
         # step 4    computing the solutions
-        pass
