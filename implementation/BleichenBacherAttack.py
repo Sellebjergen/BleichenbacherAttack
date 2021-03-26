be8ee26a6824c54c4f5e20712587ae06bf6e813e
sellebjergen <sellebjergen@gmail.com>
2021-03-26 23:25:32 +0100

    phase3 has an error, in which my range of r gets so large that i cannot possible compute all the numbers.

--- implementation/BleichenBacherAttack.py
+++ implementation/BleichenBacherAttack.py
@@ -1,7 +1,27 @@
 from Crypto.Util.number import bytes_to_long, long_to_bytes
+from collections import namedtuple
 
 # TODO: Checking the type of the oracle, if we give it an url, make a request instead.
 # TODO: Can we search after for values in the blinding phase more efficiently?
+
+Interval = namedtuple("Interval", ["lower_bound", "upper_bound"])
+
+
+def safe_interval_insert(M_new, interval):
+    for i, (a, b) in enumerate(M_new):
+
+        # overlap found, construct the larger interval
+        if (b >= interval.lower_bound) and (a <= interval.upper_bound):
+            lb = min(a, interval.lower_bound)
+            ub = max(b, interval.upper_bound)
+
+            M_new[i] = Interval(lb, ub)
+            return M_new
+
+    # no overlaps found, just insert the new interval
+    M_new.append(interval)
+
+    return M_new
 
 
 class BleichenBacherAttack:
@@ -9,6 +29,14 @@ class BleichenBacherAttack:
         self.cipher_bytes = cipher_bytes
         self.rsa = rsa
         self.oracle = oracle
+        self.rsa_e = rsa.get_public_key().e
+        self.rsa_n = rsa.get_public_key().n
+
+    def floor(self, a, b):
+        return a // b
+
+    def ceil(self, a, b):
+        return a // b + (a % b > 0)
 
     def blinding_phase(self, lower_bound, upper_bound):
         print("starting blinding phase")
@@ -16,6 +44,7 @@ class BleichenBacherAttack:
         n = self.rsa.get_public_key().n
         c = bytes_to_long(self.cipher_bytes)
         i = 1
+        # TODO: This should probably just be a search with random values instead, but it seems to work really well.
         for s_i in range(lower_bound, upper_bound):
             if s_i % 100000 == 0:
                 print("I'm still alive, don't worry about it man...")
@@ -25,42 +54,65 @@ class BleichenBacherAttack:
 
             if self.oracle.get_conforming_status(c_0_bytes):
                 print("found a value")
-                return c_0, s_i, i
+                return c_0, i
 
         # TODO: should maybe raise an exception?
         return None
 
-    def phase2_a(self, c_0, s_i, i):
+    def phase2_a(self, c_0):
         B = 2 ** (8 * (self.rsa.amount_of_bits - 2))
-        e = self.rsa.get_public_key().e
-        n = self.rsa.get_public_key().n
-
-        if i == 1:
-            s_1 = n // 3*B + 1
-            math = c_0 * pow(s_1, e, n) % n
+        s_1 = self.rsa_n // 3*B + 1
+        math = c_0 * pow(s_1, self.rsa_e, self.rsa_n) % self.rsa_n
+        math_bytes = long_to_bytes(math)
+        while not self.oracle.get_conforming_status(math_bytes):
+            s_1 += 1
+            math = c_0 * pow(s_1, self.rsa_e, self.rsa_n) % self.rsa_n
             math_bytes = long_to_bytes(math)
-            while not self.oracle.get_conforming_status(math_bytes):
-                s_1 += 1
-                math = c_0 * pow(s_1, e, n) % n
-                math_bytes = long_to_bytes(math)
-            print("We've found the next value in the interval.")
-            return s_1
+        print("We've found the next value in the interval.")
+        return s_1
+
+    def phase3(self, B, M, s_i):
+        M_new = []
+        for a, b in M:
+            r_low = self.ceil(a * s_i - 3 * B + 1, self.rsa_n)
+            r_up = self.ceil(b * s_i - 2 * B, self.rsa_n)
+
+            print(f"We have a range on r of: {r_up - r_low}")
+
+            for r in range(r_low, r_up):
+                lower_bound = max(a, self.ceil(2 * B + r * self.rsa_n, s_i))
+                upper_bound = min(b, self.floor(3 * B - 1 + r * self.rsa_n, s_i))
+
+                interval = Interval(lower_bound, upper_bound)
+
+                M_new = safe_interval_insert(M_new, interval)
+        M.clear()
+        return M_new
 
     def run(self):
         B = 2 ** (8 * (self.rsa.amount_of_bits - 2))
         lower_bound = 2*B
         upper_bound = 3*B - 1
+        M = [Interval(lower_bound, upper_bound)]
+
+        # TODO: check if the cipher by itself is PKCS conforming. Skip step 1 in such case.
 
         # step 1.   blinding
-        c_0, s_i, i = self.blinding_phase(lower_bound, upper_bound)
+        c_0, i = self.blinding_phase(lower_bound, upper_bound)
 
-        print("jumping to phase 2 instead.")
-        s_1 = self.phase2_a(c_0, s_i, i)
-
-        print(s_1)
         # step 2.   searching for PKCS conforming messages
         # step 2.a  starting the search
+        print("jumping to phase 2 instead.")
+        s_1 = 0
+        if i == 1:
+            s_1 = self.phase2_a(c_0)
+
+        print("done with phase 2")
+
+        # step 3    narrowing the set of solutions
+        self.phase3(B, M, s_1)
+        print("yeah we got through.")
+
         # step 2.b  searching with more than one interval
         # step 2.c  searching with one interval left
-        # step 3    narrowing the set of solutions
         # step 4    computing the solutions
---
+++ implementation/lib/RSA_toy.py
@@ -0,0 +1,17 @@
+class RSA_toy:
+    def __init__(self, amount_of_bits):
+        self.e = 3
+
+        pass
+
+    def encrypt(self):
+        pass
+
+    def decrypt(self):
+        pass
+
+    def get_public_key(self):
+        pass
+
+    def get_amount_of_bits(self):
+        pass
