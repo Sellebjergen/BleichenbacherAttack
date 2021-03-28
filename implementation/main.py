a40364e4c2547d60aabc923902a7e8b6e07a0b8c
sellebjergen <sellebjergen@gmail.com>
2021-03-28 22:49:35 +0200

    Everything seems to be working, I get some random bits in front of the decrypted string... low priority to remove.

--- implementation/main.py
+++ implementation/main.py
@@ -58,26 +58,47 @@ def phase2b(c, s_i):
         s_i += 1
 
 
-def phase2c(c_0, M):
-    a, b = M[0]
-    return 0, 0
+def phase2c(c_0, s_i, interval):
+    print("starting phase 2c")
+    a, b = interval
+    print(f"value of a: {a}")
+    print(f"value of b: {b}")
+    r_i = ceil(2 * (b * s_i - 2 * B), rsa_n)
+    while True:
+        lower_bound = ceil(2*B + r_i * rsa_n, b)
+        higher_bound = ceil(3*B + r_i * rsa_n, a)
+        if lower_bound > higher_bound:
+            print(f"difference: {higher_bound - lower_bound}")
+            exit(1)
+        for s in range(lower_bound, higher_bound):
+            math = c_0 * pow(s, rsa_e, rsa_n) % rsa_n
+            math_bytes = long_to_bytes(math)
+            if oracle.get_conforming_status(math_bytes):
+                print("found a suitable values of r_i and s_i")
+                return s
+        r_i += 1
 
 
 def phase3(s, M):
+    print(f"there are {len(M)} intervals here in phase 3.")
+    print("starting phase 3")
     new_M = []
     for a, b in M:
-        lower_r = (a * s - 3 * B + 1) // rsa_n
-        higher_r = (b * s - 2 * B) // rsa_n
+        lower_r = ceil(a * s - 3 * B + 1, rsa_n)
+        higher_r = ceil(b * s - 2 * B, rsa_n)
         print("Differences in r higher and lower:")
         print(higher_r - lower_r)
         for r in range(lower_r, higher_r):
-            temp_interval = Interval(max(a, (2 * B + r * rsa_n) // s), min(b, (3 * B - 1 + r * rsa_n) // s))
+            lower_bound = max(a, ceil(2 * B + r * rsa_n, s))
+            higher_bound = min(b, floor(3 * B - 1 + r * rsa_n, s))
+            temp_interval = Interval(lower_bound, higher_bound)
             new_M.append(temp_interval)
     print(f"we've defined some new intervals there are now: {len(new_M)}")
     return new_M
 
 
 def bleichenbacher(cipher_bytes):
+    global s_i
     print("starting attack.")
     cipher_integer = bytes_to_long(cipher_bytes)
     M = [Interval(2 * B, 3 * B + 1)]
@@ -86,41 +107,32 @@ def bleichenbacher(cipher_bytes):
     # Step 1.   Blinding phase.
     # Is not necessary for now, as we know it's an encrypted message we're getting.
 
-    print("step 1 finished.")
+    # print("step 1 finished.")
 
+    while True:
+        # Doing step 2
+        if i == 1:
+            s_i = phase2a(cipher_integer)
+        elif i > 1 and len(M) >= 2:
+            s_i = phase2b(cipher_integer, s_i + 1)
+        elif len(M) == 1:
+            s_i = phase2c(cipher_integer, s_i, M[0])
 
-    # TODO: this should be in a forloop, with i as the increment.
-    # step 2.a  First iteration
-    s_i = -1
-    if i == 1:
-        s_i = phase2a(cipher_integer)
-    elif i > 1 and len(M) >= 2:
-        s_i = phase2b(cipher_integer, s_i + 1)
-    elif len(M) == 1:
-        a, b = M[0]
-        r_i, s_i = phase2c()
+        # Doing step 3
+        M = phase3(s_i, M)
 
-    print("step 2 finished.")
-    print("We're done")
-
-    if s_i == -1:
-        # TODO: raise some kind of exception.
-        print("An error has happened. The value of s_1 is not set incorrect.")
-
-    # step 3.   Narrowing set of solutions.
-    M = phase3(s_i, M)
-
-    # step 4.   Computing the solution.
-    if len(M) == 1 and M[0].lower_bound == M[0].upper_bound:
-        # TODO: still need to implement the values to return.
-        result = M[0].lower_bound % rsa_n
-        return result
+        # Doing step 4
+        if len(M) == 1 and M[0].lower_bound == M[0].upper_bound:
+            result = M[0].lower_bound % rsa_n
+            return long_to_bytes(result)
+        i += 1
+        print("Doing another iteration of step 2.")
 
 
 if __name__ == "__main__":
     # Doing some setup.
-    msg = "secret message"
+    msg = "another secret message"
     msg_encrypted_bytes = rsa.encrypt(msg)
 
     # Running the actual attack.
-    bleichenbacher(msg_encrypted_bytes)
+    print(bleichenbacher(msg_encrypted_bytes))
